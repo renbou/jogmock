85b37343b7a9d85d1642bc550a40ae2bd88f4f86
renbou <artemmikheev1311@gmail.com>
2022-04-02 05:06:59 +0300

    docs: added readme and example config

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-# strava-keker
+# Jogmock
+A tool providing fake activity generation and uploads to online fitness trackers (currently only Strava is supported).
 
-https://gpx.studio/
-https://www.unixtimestamp.com/
+## Disclaimer
+By using this tool you agree to follow all the steps listed below or confirm your actions with me if they differ from the steps.
+
+## How to
+1. Move config-example.yml to config.yml and change the variables to match your device. The following mobile versions can be used (or can be found in Mobile App -> Settings -> About) for the `mobile_app_version` version. Pick a random one, please.
+   - 248.8(1223885)
+   - 247.10(1223782)
+   - 246.11(1223676)
+   - 245.9(1223569)
+   - 244.10(1223463)
+   - 243.9(1223356)
+   - 242.10(1223251)
+   - 241.8(1223144)
+   - 240.9(1223041)
+   - 238.9(1223040)
+   - 238.7(1222828)
+   - 237.5(1222722)
+   - 233.10(1222306)
+   - 233.9(1222305)
+   - 233.8(1222304)
+   - 231.9(1222094)
+   - 230.10(1221988)
+2. Use [gpx.studio](https://gpx.studio/) to draw your routes:
+   1. Click `New GPX` at the top.
+   2. Select Activity `run/hike` in the popup that appears on the left.
+   3. Make sure that `Routing (follow roads)` is **NOT** selected.
+   4. Click the minus on the right side of the popup to hide it.
+   5. Click on the screen to add points, then export the file using Export (top of the screen) -> Download.
+3. Using the modified config.yml and the downloaded file, you can start using Jogmock:
+   1. Download the release appropriate to your OS and architecture.
+   2. Install the bundled APK on your device. It is needed to get a reCAPTCHA token suitable for use with Strava and which will be needed during the first time you use `jogmock`.
+   3. Launch the tool in your terminal and simply follow the steps. During GPX selection you can use Tab to autocomplete file paths.
+
+## Help
+### Config
+The `run_activity` and `ride_activity` configs specify options used to generate the speed during an activity. You just need to specify the desired speed and `jogmock` will generate everything automatically, and these parameters can be used to fine-tune the generation in order for it to look as real (to you) as possible. Proper description of each option is specified in the example config.
+### 
---
+++ research/get-mobile-versions.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+import requests
+import re
+import os
+import tempfile
+import yaml
+import shutil
+import argparse
+from zenlog import log
+from bs4 import BeautifulSoup as bs
+
+TMP_STRAVA_APK = "strava.apk"
+
+def meta_constructor(loader, node):
+    value = loader.construct_mapping(node)
+    return value
+yaml.add_constructor(u'tag:yaml.org,2002:brut.androlib.meta.MetaInfo', meta_constructor)
+
+def unpack_apk(path: str):
+  os.system(f"apktool d -s {path}")
+  return path[:-4]
+
+def get_apk_version(path: str):
+  with open(os.path.join(path, "apktool.yml")) as f:
+    apktool_info = yaml.load(f)
+  name = apktool_info["versionInfo"]["versionName"]
+  code = apktool_info["versionInfo"]["versionCode"]
+  return f"{name}({code})"
+
+def download_and_extract_version_info(link):
+  print("Downloading file from", link)
+  r = s.get(link)
+
+  pwd = os.getcwd()
+  tmpd = tempfile.mkdtemp()
+  os.chdir(tmpd)
+  with open(TMP_STRAVA_APK, "wb") as f:
+    f.write(r.content)
+  
+  unpacked = unpack_apk(TMP_STRAVA_APK)
+  version = get_apk_version(unpacked)
+
+  os.chdir(pwd)
+  shutil.rmtree(tmpd)
+  return version
+
+s = requests.session()
+s.headers.update({"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0"})
+
+def find_apkmirror_version_links(page):
+  r = s.get(f"https://www.apkmirror.com/?post_type=app_release&searchtype=apk&page={str(page)}&s=com.strava")
+  soup = bs(r.text, "html.parser")
+  return list(map(
+    lambda tag: tag["href"],
+    filter(
+      lambda tag: "/strava-inc/" in tag["href"],
+      soup.select("a.downloadLink"))))
+
+def parse_link_version(link):
+  return re.search(r"(\d{1,5})-(\d{1,5})", link).group(0)
+
+def get_apkmirror_download_link(version):
+  r = s.get(f"https://www.apkmirror.com/apk/strava-inc/strava-running-and-cycling-gps/strava-running-and-cycling-gps-{version}-release/strava-track-running-cycling-swimming-{version}-android-apk-download/")
+  soup = bs(r.text, "html.parser")
+  links = soup.select("link[rel=shortlink]")
+  for link in links:
+    if link["href"].startswith("/?p="):
+      linkid = link["href"][len("/?p="):]
+      return f"https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id={linkid}&forcebaseapk=true"
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-p", "--page", dest="page", help="Page on APKMirror to get versions from.", default=1, type=int)
+  args = parser.parse_args()
+
+  version_links = find_apkmirror_version_links(args.page)
+  log.debug("found %d Strava versions on APKMirror: %s", len(version_links), version_links)
+  versions = list(map(parse_link_version, version_links))
+  log.debug("extracted versions from links: %s", versions)
+  download_links = list(map(get_apkmirror_download_link, versions))
+  log.debug("got download links: %s", download_links)
+  versions = []
+  for link in download_links:
+    try:
+      version = download_and_extract_version_info(link)
+      log.info("extracted version %s", version)
+      versions.append(version)
+    except:
+      pass
+  log.info("all extracted versions: %s", versions)
+
+if __name__ == "__main__":
+  main()
